2afddf611af73bd381f1b03d2c929ddfdbc6780b
Amir Tadrisi <amirtds@gmail.com>
2024-06-07 14:52:31 -0500

    chore: python3 upgrade: replace django url with re_path

--- accredible_certificate/urls.py
+++ accredible_certificate/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from views import request_certificate
 from views import update_certificate
 
 
 urlpatterns = [
-    url(r'^request_certificate$', request_certificate, name='request_certificate'),
-    url(r'^update_certificate$', update_certificate, name='update_certificate')
+    re_path(r'^request_certificate$', request_certificate, name='request_certificate'),
+    re_path(r'^update_certificate$', update_certificate, name='update_certificate')
 ]
