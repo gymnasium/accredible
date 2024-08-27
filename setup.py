5bd8493597f7cae1a051c9c1fe74e608d1b629ed
Amir Tadrisi <amirtds@gmail.com>
2024-08-27 15:09:08 -0500

    chore: bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='accredible_certificate',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='Reporting and data retrieval for Open edX',
     entry_points={
